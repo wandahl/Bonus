678266af2a51d7d7ef99733b5d44936340e2c97c
wandahl-mac <olof.wandahl@gmail.com>
2013-05-21 16:04:40 +0200

    Uppg 1 completed

--- Bonus_1/src/Changer.java
+++ Bonus_1/src/Changer.java
@@ -1,12 +1,24 @@
 
 public class Changer {
-
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] sekValues = {1, 5, 10, 20, 50, 100, 500, 1000};
+		int[] sekValuesTest = { 1, 5, 10 };
+		int[] specialFall = {4, 5, 6};
+		System.out.println(change(7,specialFall,specialFall.length));
+	}
+	
+	public static int change( int amount, int[] val, int n ){
+		if(amount==0){
+			return 1;
+		}
+		if(amount<0){
+			return 0;
+		}
+		if(n<=0 && amount>=1){
+			return 0;
+		}
+		return change(amount,val,n-1) + change(amount-val[n-1],val,n);
 	}
 
 }
